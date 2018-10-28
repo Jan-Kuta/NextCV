4974e018e7a85b3b6de4a55717875ea6e4191859
Jan-Kuta <jan.kuta@email.cz>
2018-10-28 09:24:01 +0100

    Solved bugs in Authentication for providers

--- pages/connect.js
+++ pages/connect.js
@@ -1,30 +1,22 @@
 import React, {Component}  from 'react';
 import Router, {withRouter} from 'next/router'
-import {authenticateProvider} from '../lib/auth';
+import {authenticateProvider, setToken} from '../lib/auth';
 
 export class Connect extends Component { // eslint-disable-line react/prefer-stateless-function
-  constructor(){
-    super();
-    this.state = { error: null }
-  }
-
   componentDidMount() {
     authenticateProvider(this.props.router.query.provider, this.props.router.query)
       .then((res) => {
         Router.replace('/');
+        setToken(res);
       })
-      .catch((err) => {
-        Router.replace('/');
+      .catch(() => {
+        Router.replace('/auth/login');
       })
   }
 
   render() {
-    const { error } = this.state;
     return (
       <div>
-        { error && (
-          <div className="w3-red">{error}</div>
-        )}
         <h2>Retrieving your token and checking validity</h2>
       </div>
     );
